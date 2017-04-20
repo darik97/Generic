efbed81ffe8b99997cae32778c24bab3a2a46d35
Daria <dara.darik@gmail.com>
2017-04-20 18:18:46 +0500

    added maxHeap

---
+++ BinaryHeap/BinaryHeap/MaxBinaryHeap.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryHeap
+{
+    class MaxBinaryHeap<T>
+        where T : IComparable
+    {
+        public readonly List<T> heap;
+        public int Size
+        {
+            get
+            {
+                return heap.Count();
+            }
+        }
+
+        public MaxBinaryHeap()
+        {
+            heap = new List<T>();
+        }
+
+        public void Insert(T value)
+        {
+            heap.Add(value);
+            int i = Size - 1;
+            int parent = (i - 1) / 2;
+            while (i > 0 && heap[parent].CompareTo(heap[i]) < 0)
+            {
+                T temp = heap[i];
+                heap[i] = heap[parent];
+                heap[parent] = temp;
+                i = parent;
+                parent = (i - 1) / 2;
+            }
+        }
+
+        public T Extract()
+        {
+            if (Size > 0)
+            {
+                T value = heap[0];
+                heap[0] = heap[Size - 1];
+                heap.RemoveAt(Size - 1);
+                heapify();
+                return value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        
+        void heapify()
+        {
+            int left;
+            int right;
+            int max;
+            int i = 0;
+            
+            for (; ; )
+            {
+                left = 2 * i + 1;
+                right = 2 * i + 2;
+                max = i;
+
+                if (left < Size && heap[left].CompareTo(heap[max]) > 0)
+                {
+                    max = left;
+                }
+                if (right < Size && heap[right].CompareTo(max) > 0)
+                {
+                    max = right;
+                }
+                if (max == i)
+                {
+                    break;
+                }
+
+                T temp = heap[i];
+                heap[i] = heap[max];
+                heap[max] = temp;
+                i = max;
+            } 
+        }        
+    }
+}
---
+++ BinaryHeap/BinaryHeap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxBinaryHeap<int> maxHeap = new MaxBinaryHeap<int>();
+            maxHeap.Insert(0);
+            maxHeap.Insert(1);
+            maxHeap.Insert(22);
+            maxHeap.Insert(13);
+            maxHeap.Insert(10);
+            maxHeap.Insert(31);
+            maxHeap.Insert(15);
+            maxHeap.Insert(3);
+            for (int i = 0; i < maxHeap.heap.Count; i++)
+            {
+                Console.WriteLine(maxHeap.heap[i]);
+            }
+        }
+    }
+}
